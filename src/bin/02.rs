9757269c14c5606c730bfc44987b2add128539d2
Wang Ruochen <wrc@ruo-chen.wang>
2021-01-01 19:51:13 +0800

    Add day 2

---
+++ src/bin/02.rs
@@ -0,0 +1,47 @@
+fn validate1(line: &str) -> Option<bool> {
+    let mut sp = line.splitn(3, ' ');
+    let times = sp.next()?;
+    let ch = sp.next()?.chars().nth(0)?;
+    let passw = sp.next()?;
+
+    let mut sp = times.splitn(2, '-');
+    let min: usize = sp.next()?.parse().ok()?;
+    let max: usize = sp.next()?.parse().ok()?;
+
+    let cnt = passw.chars().filter(|&c| c == ch).count();
+    Some(cnt >= min && cnt <= max)
+}
+
+fn validate2(line: &str) -> Option<bool> {
+    let mut sp = line.splitn(3, ' ');
+    let times = sp.next()?;
+    let ch = sp.next()?.chars().nth(0)?;
+    let mut passw = sp.next()?.chars();
+
+    let mut sp = times.splitn(2, '-');
+    let p1: usize = sp.next()?.parse().ok()?;
+    let p2: usize = sp.next()?.parse().ok()?;
+
+    let c1 = passw.nth(p1 - 1)?;
+    let c2 = passw.nth(p2 - p1 - 1)?;
+
+    Some((c1 == ch) ^ (c2 == ch))
+}
+
+fn count_good(lines: &[String], validate: fn(&str) -> Option<bool>) -> usize {
+    lines
+        .iter()
+        .flat_map(|li| validate(&li))
+        .filter(|&x| x)
+        .count()
+}
+
+fn main() {
+    let lines: Vec<_> = aoc_2020::stdin_lines().collect::<Result<_, _>>().unwrap();
+
+    let x = count_good(&lines, validate1);
+    println!("part1: {}", x);
+
+    let x = count_good(&lines, validate2);
+    println!("part2: {}", x);
+}
